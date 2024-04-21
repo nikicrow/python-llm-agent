3bf62c01e521a74f6f192d032087f3a275ea3ae8
nikicrow <nikiallum94@gmail.com>
2024-04-21 14:31:18 +1000

    add data and update app

--- app.py
+++ app.py
@@ -1,20 +1,5 @@
-from langchain.memory import ConversationBufferMemory 
-from langchain.agents import AgentExecutor
-from langchain_community.chat_models import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
-from langchain.tools.render import format_tool_to_openai_function
-from langchain.agents.format_scratchpad import format_to_openai_functions
-from langchain.schema.runnable import RunnablePassthrough
-from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
-from langchain.prompts import MessagesPlaceholder
+from openai import OpenAI
 import streamlit as st
-from langchain_experimental.tools import PythonREPLTool
-from langchain.memory import ChatMessageHistory
-from langchain.agents import load_tools
-from langchain import hub
-from langchain.agents import AgentExecutor
-from langchain_experimental.tools import PythonREPLTool
-from langchain.agents import create_openai_functions_agent
 
 
 # page config
@@ -29,19 +14,6 @@ openai_key = st.secrets["OPEN_AI_KEY"]
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-# add memory to the chat for conversation history
-formatted_chat_history = ChatMessageHistory()
-for message in st.session_state.chat_history:
-    if message['role']=='user':
-        formatted_chat_history.add_user_message(message['content'])
-    elif message['role']=='assistant':
-        formatted_chat_history.add_ai_message(message['content'])
-memory = ConversationBufferMemory(chat_memory=formatted_chat_history,
-                                    return_messages=True,
-                                    memory_key="chat_history",
-                                    output_key="output")
-tools = [PythonREPLTool()]
-
 instructions = """You are an agent designed to write and execute python code to answer questions.
 You have access to a python REPL, which you can use to execute python code.
 If you get an error, debug your code and try again.
@@ -50,51 +22,34 @@ You might know the answer without running any code, but you should still run the
 Always put the code you wrote in the output and print any images of charts you create.
 If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
 """
-base_prompt = hub.pull("langchain-ai/openai-functions-template")
-prompt = base_prompt.partial(instructions=instructions)
-# load model from open AI
-llm = ChatOpenAI(model_name = "gpt-3.5-turbo-0125",
-                    openai_api_key = openai_key,
-                    temperature = 0.2, 
-                    streaming=True)
-
-agent = create_openai_functions_agent(llm, 
-                                      tools, 
-                                      prompt)
-
-agent_executor = AgentExecutor(agent=agent, 
-                               tools=tools, 
-                               verbose=True,
-                               memory=memory, 
-                               return_intermediate_steps=True)
-
 
 def display_chat():
     for entry in st.session_state.chat_history:
         with st.chat_message(entry["role"]):
-            st.write(entry["thoughts"])
             st.write(entry["content"])
 
 # function to get model answer
-def create_answer(question):
+def create_answer():
     # create answer
-    result = agent_executor.invoke({'input':question})
-    # add that message too the chat history
-    thoughts = result["intermediate_steps"][0]
-    st.write
+    client = OpenAI()
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=st.session_state.chat_history,
+        temperature=0.7,
+        top_p=1
+    )
+    # add that response to the chat history
     st.session_state.chat_history.append({
         "role": "assistant",
-        "thoughts" : thoughts,
-        "content": result["output"],
+        "content": response.choices[0].message.content,
     })
 
 # If we get a question, start doing stuff!
 if question := st.chat_input(placeholder="Let's chat"):
     st.session_state.chat_history.append({
         "role": "user",
-        "thoughts":"",
         "content": question,
     })
-    create_answer(question)
+    create_answer()
     display_chat()
     
